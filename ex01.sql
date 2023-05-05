c95d45a9b6c5c25ba9f29e48db66e9177c4b433d
Guilherme-07062002 <guilherme762002@gmail.com>
2023-05-05 16:24:18 -0300

    ex03

--- ex01.sql
+++ ex01.sql
@@ -1,17 +1,16 @@
-CREATE
-OR REPLACE FUNCTION consulta_aluno(nome varchar(50)) RETURNS TABLE(
-    nom_alu varchar(50),
-    nom_curso varchar(50),
-    tot_cred integer,
-    mgp float
-) AS $ $
-SELECT
-    alunos.nom_alu,
-    cursos.nom_curso,
-    alunos.tot_cred,
-    alunos.mgp
-FROM
-    alunos
-    join cursos on alunos.cod_curso = cursos.cod_curso
-where
-    nom_alu ilike '%' || nome || '%';$ $ LANGUAGE SQL;
+CREATE OR REPLACE FUNCTION 
+    consulta_aluno(nome VARCHAR(50)) RETURNS TABLE(
+        nom_alu VARCHAR(50),
+        nom_curso VARCHAR(50),
+        tot_cred INTEGER,
+        mgp FLOAT
+) AS $$
+    SELECT
+        alunos.nom_alu,
+        cursos.nom_curso,
+        alunos.tot_cred,
+        alunos.mgp
+        FROM alunos
+        JOIN cursos ON alunos.cod_curso = cursos.cod_curso
+        WHERE nom_alu ilike '%' || nome || '%';
+$$ LANGUAGE SQL;
