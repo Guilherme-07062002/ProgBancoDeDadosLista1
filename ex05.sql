ef3e8c392516ad5c1963a41520a5ad5e4097d5ce
Guilherme-07062002 <guilherme762002@gmail.com>
2023-05-05 16:49:18 -0300

    ex05

---
+++ ex05.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION disciplinas_dependentes(codigo integer)
+RETURNS TABLE(dependentes varchar(50))
+AS $$
+	SELECT nom_disc 
+	FROM disciplinas
+	WHERE cod_disc IN 
+		(SELECT cod_disc 
+		FROM pre_requisitos 
+		WHERE cod_disc_pre = codigo);
+$$
+LANGUAGE SQL;
